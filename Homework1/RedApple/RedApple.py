2d82bc3ddf9c234c7e01cdf9bbaca120f2b6c0de
Treekay <823334594@qq.com>
2018-10-01 23:16:09 +0800

    .

--- Homework1/RedApple/RedApple.py
+++ Homework1/RedApple/RedApple.py
@@ -3,11 +3,17 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+def findMedian(tubes,medianNum):
+    for i in range(len(tubes)):
+        if (medianNum-tubes[i][1]<=0):
+            return int(tubes[i][0])
+        else: medianNum -= tubes[i][1]
+
 img = np.array(Image.open('../img/redapple.jpg'))
 rows, cols, dims = img.shape
 
-plt.imshow(img)
-plt.pause(2)
+#plt.imshow(img)
+#plt.pause(2)
 
 # pick up all the pixels
 pixels = []
@@ -23,8 +29,9 @@ for i in range(256):
     channel[G].append(0)
     channel[B].append(0)
 
+print(len(pixels))
 # statistics the num of each r,g,b
-for i in range(pixels.size()):
+for i in range(len(pixels)):
     channel[R][pixels[i][R]] += 1
     channel[G][pixels[i][G]] += 1
     channel[B][pixels[i][B]] += 1
@@ -32,13 +39,11 @@ for i in range(pixels.size()):
 # get the ColorNumTuple
 tube = [[],[],[]]
 for i in range(3):
-    for t in range(channel[i].size()):
+    for t in range(len(channel[i])):
         if (channel[i][t] != 0):
-            tube[i].append(tuple(t,channel[i][t]))
+            tube[i].append([t,channel[i][t]])
 
-sorted(tube[R], key=lambda tube[R] : tube[R][1])
-sorted(tube[G], key=lambda tube[G] : tube[G][1])
-sorted(tube[B], key=lambda tube[B] : tube[B][1])
+print(tube[R],'\n',tube[G],'\n',tube[B])
+medianNum = len(pixels)//2
 
-def divideByRchannel(tubes):
-    
+print(findMedian(tube[R],medianNum),findMedian(tube[G],medianNum),findMedian(tube[B],medianNum))
