f5a139a26cd9f256007daf863b32268c6c60c05b
Treekay <823334594@qq.com>
2018-10-06 14:39:30 +0800

    red channel only for requirement

--- Homework1/IrisWipe/IrisWipe.py
+++ Homework1/IrisWipe/IrisWipe.py
@@ -5,9 +5,22 @@ import matplotlib.pyplot as plt
 import cv2
 
 # load the images and keep its data in np.array type
-im1 = np.array(Image.open('../img/诺贝尔.jpg'))
-im2 = np.array(Image.open('../img/lena.jpg'))
-rows, cols, dims = im1.shape
+img1 = Image.open('../img/诺贝尔.jpg')
+img2 = Image.open('../img/lena.jpg')
+
+'''
+#only show in red channel
+r1, g1, b1 = img1.split()
+r2, g2, b2 = img2.split()
+img1 = r1
+img2 = r2
+'''
+
+im1 = np.array(img1)
+im2 = np.array(img2)
+
+#rows, cols = im1.shape  #for red channel only
+rows, cols, dims = im1.shape #for three channels
 
 # initial the counting time
 t = 0
@@ -17,8 +30,8 @@ while t < 500:
         for y in range(cols):
             # the radius decide by the time
             if (pow(x-rows//2,2)+pow(y-cols//2,2)<=pow(t,2)):
-                # change the circle region to lena.jpg 
-                im1[x,y,:] = im2[x,y,:]
+                # change the circle region to lena
+                im1[x,y] = im2[x,y]
     t += 40
     plt.imshow(im1)
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None) # 去除白边
