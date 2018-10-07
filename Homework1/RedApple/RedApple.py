479771cf48a2adb0b24857c0d076c9a30f849479
Treekay <823334594@qq.com>
2018-10-07 19:12:31 +0800

    fix

--- Homework1/RedApple/RedApple.py
+++ Homework1/RedApple/RedApple.py
@@ -1,7 +1,5 @@
 import operator
-import math
 import cv2
-import sys
 
 def ImgProcess(img):
     rows, cols, dims = img.shape
@@ -13,26 +11,30 @@ def ImgProcess(img):
         for y in range(cols):
             pixels.append([img[x,y,B],img[x,y,G],img[x,y,R],x,y])
 
-    '''
     # determine the 256 present color
     medianPos = len(pixels)
     pixels.sort(key=operator.itemgetter(R))
     for t in range(8):
-        medianPos = medianPos//2
-        ltimes = len(pixels) // medianPos
+        ltimes = len(pixels)//medianPos
         if (t%3 == 0):
             for i in range(ltimes):
-                pixels[i*medianPos:(i+1)*medianPos].sort(key=operator.itemgetter(R))
+                temp = pixels[i*medianPos:(i+1)*medianPos]
+                temp.sort(key=operator.itemgetter(R))
+                pixels[i*medianPos:(i+1)*medianPos] = temp
         elif (t%3 == 1):
             for i in range(ltimes):
-                pixels[i*medianPos:(i+1)*medianPos].sort(key=operator.itemgetter(G))
+                temp = pixels[i*medianPos:(i+1)*medianPos]
+                temp.sort(key=operator.itemgetter(G))
+                pixels[i*medianPos:(i+1)*medianPos] = temp
         elif (t%3 == 2):
             for i in range(ltimes):
-                pixels[i*medianPos:(i+1)*medianPos].sort(key=operator.itemgetter(B))
+                temp = pixels[i*medianPos:(i+1)*medianPos]
+                temp.sort(key=operator.itemgetter(B))
+                pixels[i*medianPos:(i+1)*medianPos] = temp
+        medianPos = medianPos//2
 
     region = len(pixels)//256
     for t in range(256):
-        presentColor = [[],[],[]]
         Rsum = 0
         Gsum = 0
         Bsum = 0
@@ -40,16 +42,14 @@ def ImgProcess(img):
             Rsum += pixels[i][R]
             Gsum += pixels[i][G]
             Bsum += pixels[i][B]
-        presentColor[R] = Rsum // region
-        presentColor[G] = Gsum // region
-        presentColor[B] = Bsum // region
+        presentColor = [Bsum//region,Gsum//region,Rsum//region]
         for i in range(t*region,(t+1)*region):
             x = pixels[i][3]
             y = pixels[i][4]
             img[x,y,R] = presentColor[R]
             img[x,y,G] = presentColor[G]
             img[x,y,B] = presentColor[B]
-    '''
+
     # save the new img
     cv2.imwrite("../res/hw2.jpg",img)
 
