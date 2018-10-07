555f2237e3f46a752b75d7e260fa6159f3d173f5
Treekay <823334594@qq.com>
2018-10-07 23:03:07 +0800

    a bit faster?

--- Homework1/IrisWipe/IrisWipe.py
+++ Homework1/IrisWipe/IrisWipe.py
@@ -5,38 +5,33 @@ import matplotlib.pyplot as plt
 import cv2
 
 def IrisWipe(img1, img2):
-
-    '''
-    #only show in red channel
-    r1, g1, b1 = img1.split()
-    r2, g2, b2 = img2.split()
-    img1 = r1
-    img2 = r2
-    '''
-
     im1 = np.array(img1)
     im2 = np.array(img2)
-
-    #for red channel only
-    #rows, cols = im1.shape
-
-    #for three channels
     rows, cols, dims = im1.shape
 
-    # initial the counting time
+    # initial
+    increment = cols//10
+    centre = cols//2-1
     t = 0
-    while t < 500:
-        # check each pixel by orws and cols
-        for x in range(rows):
-            for y in range(cols):
-                # the radius decide by the time
-                if (pow(x-rows//2,2)+pow(y-cols//2,2)<=pow(t,2)):
+    while t < 20:
+        # the radius decide by the time
+        r = 40*t
+
+        lowbound = centre-increment*t
+        upbound = centre+increment*t
+        if (lowbound < 0):
+            lowbound = 0
+        if (upbound > cols - 1):
+            upbound = cols - 1
+
+        for x in range(lowbound,upbound):
+            for y in range(lowbound,upbound):
+                if (pow(x-centre,2)+pow(y-centre,2)<=pow(r,2)):
                     # change the circle region to lena
                     im1[x,y] = im2[x,y]
-        t += 40
+        t += 1
         plt.imshow(im1)
         plt.axis('off') # 去除坐标系
-
         # keep a while show the changing process
         plt.pause(0.03)
     plt.close()
