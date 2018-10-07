82419bd799d0278e69bc31ac1e1a2c3c609bc8fa
Treekay <823334594@qq.com>
2018-10-07 23:55:53 +0800

    still working

--- Homework1/IrisWipe/IrisWipe.py
+++ Homework1/IrisWipe/IrisWipe.py
@@ -1,8 +1,6 @@
 from PIL import Image
-import math
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
+import math
 
 def IrisWipe(img1, img2):
     im1 = np.array(img1)
@@ -10,31 +8,24 @@ def IrisWipe(img1, img2):
     rows, cols, dims = im1.shape
 
     # initial
-    increment = cols//10
-    centre = cols//2-1
     t = 0
-    while t < 20:
+    frames = []
+    while t < 11:
         # the radius decide by the time
         r = 40*t
-
-        lowbound = centre-increment*t
-        upbound = centre+increment*t
-        if (lowbound < 0):
-            lowbound = 0
-        if (upbound > cols - 1):
-            upbound = cols - 1
-
-        for x in range(lowbound,upbound):
-            for y in range(lowbound,upbound):
-                if (pow(x-centre,2)+pow(y-centre,2)<=pow(r,2)):
+        for x in range(cols):
+            for y in range(rows):
+                if (pow(x-rows//2-1,2)+pow(y-rows//2-1,2)<=pow(r,2)):
                     # change the circle region to lena
                     im1[x,y] = im2[x,y]
         t += 1
-        plt.imshow(im1)
-        plt.axis('off') # 去除坐标系
-        # keep a while show the changing process
-        plt.pause(0.03)
-    plt.close()
+        # append the temp img
+        frames.append(im1)
+
+    # create a gif
+        frames[0].save('../res/IrisWipe.gif', )
+    # Show the gif
+        
 
 # main()
 img1 = Image.open('../img/Nobel.jpg')
