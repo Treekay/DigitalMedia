846603339d9b947ef54a73af9a4cbec926388de7
Treekay <823334594@qq.com>
2018-10-08 00:22:54 +0800

    finish

--- Homework1/IrisWipe/IrisWipe.py
+++ Homework1/IrisWipe/IrisWipe.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import os
 import math
 
 def IrisWipe(img1, img2):
@@ -8,24 +9,24 @@ def IrisWipe(img1, img2):
     rows, cols, dims = im1.shape
 
     # initial
-    t = 0
+    r = 0
     frames = []
-    while t < 11:
-        # the radius decide by the time
-        r = 40*t
+    print("Creating...")
+    while r <= 580:
         for x in range(cols):
             for y in range(rows):
                 if (pow(x-rows//2-1,2)+pow(y-rows//2-1,2)<=pow(r,2)):
                     # change the circle region to lena
                     im1[x,y] = im2[x,y]
-        t += 1
+        r += 15
         # append the temp img
-        frames.append(im1)
+        frames.append(Image.fromarray(im1))
 
     # create a gif
-        
+    frames[0].save('../res/hw1.gif', save_all=True, append_images=frames[1:], duration=0.01, loop=0)
     # Show the gif
-        
+    os.system(r"start ../res/hw1.gif")
+    print("Success")
 
 # main()
 img1 = Image.open('../img/Nobel.jpg')
