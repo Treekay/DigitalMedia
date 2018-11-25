8a08586409a02828b34276df1cf35e3924947553
Treekay <823334594@qq.com>
2018-11-25 20:33:00 +0800

    main

--- Homework2/main.py
+++ Homework2/main.py
@@ -4,14 +4,12 @@ import JpegDecompress
 if __name__ == "__main__":
     # Compress
     compress1 = JpegCompress.Compress('./src/cartoon.jpg')
-    #compress2 = JpegCompress.Compress('./src/animal.jpg')
+    compress2 = JpegCompress.Compress('./src/animal.jpg')
     compressedData1 = compress1.getCompressedData()
-    #compressedData2 = compress2.getCompressedData()
+    compressedData2 = compress2.getCompressedData()
     # Decompress
     decompress1 = JpegDecompress.Decompress(compressedData1, './res/cartoon.jpg')
-    #decompress2 = JpegDecompress.Decompress(compressedData2, './res/animal.jpg')
+    decompress2 = JpegDecompress.Decompress(compressedData2, './res/animal.jpg')
     decompressImg1 = decompress1.getDecompressImg()
-    #decompressImg2 = decompress2.getDecompressImg()
+    decompressImg2 = decompress2.getDecompressImg()
     # Save and display image
-    
-    # Compare with origin Image and GIF-Compress-Algorithm
