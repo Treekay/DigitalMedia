7957ddc8577eb474513378fd3abc0dcd52b8b061
Treekay <823334594@qq.com>
2018-11-27 12:39:05 +0800

    finish

--- Homework2/main.py
+++ Homework2/main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from PIL import Image
 
 import JpegCompress
 import JpegDecompress
@@ -11,7 +12,7 @@ def computeDistortionRatio(src, res):
 
 if __name__ == "__main__":
     # test1
-    src1 = cv2.imread('./src/cartoon.jpg')
+    src1 = cv2.imread('./src/cartoon.bmp')
     # JPEG压缩图像
     compress1 = JpegCompress.Compress(src1)
     compressedData1 = compress1.getCompressedData()
@@ -19,9 +20,11 @@ if __name__ == "__main__":
     decompress1 = JpegDecompress.Decompress(compressedData1, './res/cartoon.jpg')
     compressedImg1 = decompress1.getDecompressImg()
     computeDistortionRatio(src1, compressedImg1)
+    gif1 = np.array(Image.open("./res/cartoon.gif").convert('RGB'))[..., ::-1]
+    computeDistortionRatio(src1, gif1)
 
     # test2
-    src2 = cv2.imread('./src/animal.jpg')
+    src2 = cv2.imread('./src/animal.bmp')
     # JPEG压缩图像
     compress2 = JpegCompress.Compress(src2)
     compressedData2 = compress2.getCompressedData()
@@ -29,3 +32,5 @@ if __name__ == "__main__":
     decompress2 = JpegDecompress.Decompress(compressedData2, './res/animal.jpg')
     compressedImg2 = decompress2.getDecompressImg()
     computeDistortionRatio(src2, compressedImg2)
+    gif2 = np.array(Image.open("./res/animal.gif").convert('RGB'))[..., ::-1]
+    computeDistortionRatio(src2, gif2)
